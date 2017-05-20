37b593c1d7b044f8cb91d20d4b28f7fda1891503
huangjiajun <sumihuang91@gmail.com>
2017-05-20 14:54:00 +0800

    clone.js
    使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝;
    被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等

--- clone.js
+++ clone.js
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head lang="en">
-  <meta charset="UTF-8">
-  <title></title>
-  <script>
+
     // 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
     // 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
     function cloneObject(src) {
@@ -61,9 +56,3 @@
 
      console.log(tarObj.a);      // 1
      console.log(tarObj.b.b1[0]);    // "hello"
-
-  </script>
-</head>
-<body>
-</body>
-</html>
